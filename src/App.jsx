bd2256fdc5d2e80f8b61f7cdbc00140958956599
Laguni <jpfsantos2004@gmail.com>
2023-08-02 23:26:14 -0300

    Homepage

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './index.css'
+import './App.css'
+import Navbar from './components/Navbar'
+import Content from './components/Content'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Content/>
+
+      
+
+    </>
+  )
+}
+
+export default App
